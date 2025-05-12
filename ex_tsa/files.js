45b00f3d8386d3c72a9b5035dbdfec7199b382e1
希铁石z <75575551+xitieshiz2@users.noreply.github.com>
2025-05-12 22:36:42 +0800

    Update files.js

--- ex_tsa/files.js
+++ ex_tsa/files.js
@@ -1,9 +1,10 @@
 {
 	"TaipeiSansTCBeta-Bold.ttf": 3304181012,
 	"TaipeiSansTCBeta-Regular.ttf": 3961277653,
-	"global.js": 3978008086,
+	"global.js": 1121871201,
 	"patch.js": 2283819644,
 	"th06.js": 4196957148,
 	"th07.js": 4196957148,
-	"th08.js": 4196957148
+	"th08.js": 4196957148,
+	"th20.v0.01a.js": 1085019511
 }
